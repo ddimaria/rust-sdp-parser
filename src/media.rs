9df6457f981de40a4c9735c95578905609fea364
David DiMaria <info@collectivesessions.com>
2022-05-03 08:26:53 -0600

    refactory media to group impls with structs

--- src/media.rs
+++ src/media.rs
@@ -37,95 +37,6 @@ pub struct Media<'a> {
     pub ssrc: Vec<Ssrc<'a>>,
 }
 
-/// Candidate
-///
-/// a=candidate:1467250027 1 udp 2122260223 192.168.0.196 46243 typ host generation 0
-///
-/// ICE is the protocol chosen for NAT traversal in WebRTC. You can find a very
-/// didactic and comprehensive explanation of ICE here. ICE is complex enough
-/// to deserve its own post, but I will try to explain its SDP lines in an
-/// understandable way.
-///
-/// Host candidate for RTP on UDP - in this ICE line our browser is giving its
-/// host candidates- the IP of the interface or interfaces the browser is listening
-/// on the computer. The browser can receive/send SRTP and SRTCP on that IP in case
-/// there is IP visibility with some candidate of the remote peer. For example, if
-/// the other computer is on the same LAN, hosts candidates will be used. The number
-/// after the protocol (udp) – 2122260223 - is the priority of the candidate. Notice
-/// that priority of host candidates is the higher than other candidates as using host
-/// candidates are more efficient in terms of use of resources. The first lines
-/// (component= 1) is for RTP and second line (component = 2) is for RTCP.
-#[derive(Debug, Default, Serialize, PartialEq)]
-pub struct Candidate<'a> {
-    pub component: u64,
-    pub foundation: &'a str,
-    pub transport: &'a str,
-    pub priority: u64,
-    pub ip: &'a str,
-    pub port: u64,
-    pub r#type: &'a str,
-}
-
-/// FMTP
-///
-/// a=fmtp:111 minptime=10; useinbandfec=1
-///
-/// This line includes optional payload-format-specific parameters supported by Chrome
-/// for audio Opus codec. minipitime=10 specifies the lowest value of the packetization
-/// time (ptime: the number of miliseconds of audio transported by a single packet).
-/// useinbandfec=1 specifies that the decoder has the capability to take advantage of
-/// the Opus in-band FEC (Forward Error Correction). For more info check RFC7587.
-#[derive(Debug, Default, Serialize, PartialEq)]
-pub struct Fmtp<'a> {
-    pub config: &'a str,
-    pub payload: u64,
-}
-
-/// RTP Map
-///
-/// a=rtpmap:111 opus/48000/2
-///
-/// Opus is one of the MTI audio codecs for WebRTC. It features a variable
-/// bit rate (6kbps-510kbps) and is not under any royalty so it can be freely
-/// implemented in any browser (unlike other codecs like as G.729). Opus
-/// support is starting to become common and it has become critical for most
-/// WebRTC applications.
-#[derive(Debug, Default, Serialize, PartialEq)]
-pub struct Rtpmap<'a> {
-    pub codec: &'a str,
-    pub payload: &'a str,
-    pub rate: u64,
-}
-
-/// RTCP FB
-///
-/// a=rtcp-fb:100 nack
-///
-/// This line requests the use of Negative ACKs (nack) as indicated in RFC 4585.
-/// This allows to make the other end aware about packet losses.
-#[derive(Debug, Default, Serialize, PartialEq)]
-pub struct RtcpFb<'a> {
-    pub payload: &'a str,
-    pub r#type: &'a str,
-}
-
-/// SSSRC
-///
-/// a=ssrc:3570614608 cname:4TOk42mSjXCkVIa6
-///
-/// The cname source attribute associates a media source with its Canonical End-Point
-/// Identifier which will remain constant for the RTP media stream even when the ssrc
-/// identifier changes if a conflict is found. This is the value that the media sender
-/// will place in its RTCP SDES packets.
-#[derive(Debug, Default, Serialize, PartialEq)]
-pub struct Ssrc<'a> {
-    pub id: u64,
-    pub attribute: &'a str,
-
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub value: Option<&'a str>,
-}
-
 impl<'a> Media<'a> {
     pub(crate) fn new(value: &'a str) -> Result<Self> {
         let mut split = value.split(' ');
@@ -160,21 +71,33 @@ impl<'a> Media<'a> {
     }
 }
 
-impl<'a> Rtpmap<'a> {
-    pub(crate) fn new(value: &'a str) -> Result<Self> {
-        let mut split = value.split(' ');
-        let payload = parse_str(split.next(), 1)?;
-
-        let mut split = parse_str(split.next(), 2)?.split('/');
-        let codec = parse_str(split.next(), 2)?;
-        let rate = parse_number::<u64>(split.next(), 3)?;
-
-        Ok(Self {
-            codec,
-            payload,
-            rate,
-        })
-    }
+/// Candidate
+///
+/// a=candidate:1467250027 1 udp 2122260223 192.168.0.196 46243 typ host generation 0
+///
+/// ICE is the protocol chosen for NAT traversal in WebRTC. You can find a very
+/// didactic and comprehensive explanation of ICE here. ICE is complex enough
+/// to deserve its own post, but I will try to explain its SDP lines in an
+/// understandable way.
+///
+/// Host candidate for RTP on UDP - in this ICE line our browser is giving its
+/// host candidates- the IP of the interface or interfaces the browser is listening
+/// on the computer. The browser can receive/send SRTP and SRTCP on that IP in case
+/// there is IP visibility with some candidate of the remote peer. For example, if
+/// the other computer is on the same LAN, hosts candidates will be used. The number
+/// after the protocol (udp) – 2122260223 - is the priority of the candidate. Notice
+/// that priority of host candidates is the higher than other candidates as using host
+/// candidates are more efficient in terms of use of resources. The first lines
+/// (component= 1) is for RTP and second line (component = 2) is for RTCP.
+#[derive(Debug, Default, Serialize, PartialEq)]
+pub struct Candidate<'a> {
+    pub component: u64,
+    pub foundation: &'a str,
+    pub transport: &'a str,
+    pub priority: u64,
+    pub ip: &'a str,
+    pub port: u64,
+    pub r#type: &'a str,
 }
 
 impl<'a> Candidate<'a> {
@@ -204,6 +127,21 @@ impl<'a> Candidate<'a> {
     }
 }
 
+/// FMTP
+///
+/// a=fmtp:111 minptime=10; useinbandfec=1
+///
+/// This line includes optional payload-format-specific parameters supported by Chrome
+/// for audio Opus codec. minipitime=10 specifies the lowest value of the packetization
+/// time (ptime: the number of miliseconds of audio transported by a single packet).
+/// useinbandfec=1 specifies that the decoder has the capability to take advantage of
+/// the Opus in-band FEC (Forward Error Correction). For more info check RFC7587.
+#[derive(Debug, Default, Serialize, PartialEq)]
+pub struct Fmtp<'a> {
+    pub config: &'a str,
+    pub payload: u64,
+}
+
 impl<'a> Fmtp<'a> {
     pub(crate) fn new(value: &'a str) -> Result<Self> {
         let mut split = value.splitn(2, ' ');
@@ -214,6 +152,51 @@ impl<'a> Fmtp<'a> {
     }
 }
 
+/// RTP Map
+///
+/// a=rtpmap:111 opus/48000/2
+///
+/// Opus is one of the MTI audio codecs for WebRTC. It features a variable
+/// bit rate (6kbps-510kbps) and is not under any royalty so it can be freely
+/// implemented in any browser (unlike other codecs like as G.729). Opus
+/// support is starting to become common and it has become critical for most
+/// WebRTC applications.
+#[derive(Debug, Default, Serialize, PartialEq)]
+pub struct Rtpmap<'a> {
+    pub codec: &'a str,
+    pub payload: &'a str,
+    pub rate: u64,
+}
+
+impl<'a> Rtpmap<'a> {
+    pub(crate) fn new(value: &'a str) -> Result<Self> {
+        let mut split = value.split(' ');
+        let payload = parse_str(split.next(), 1)?;
+
+        let mut split = parse_str(split.next(), 2)?.split('/');
+        let codec = parse_str(split.next(), 2)?;
+        let rate = parse_number::<u64>(split.next(), 3)?;
+
+        Ok(Self {
+            codec,
+            payload,
+            rate,
+        })
+    }
+}
+
+/// RTCP FB
+///
+/// a=rtcp-fb:100 nack
+///
+/// This line requests the use of Negative ACKs (nack) as indicated in RFC 4585.
+/// This allows to make the other end aware about packet losses.
+#[derive(Debug, Default, Serialize, PartialEq)]
+pub struct RtcpFb<'a> {
+    pub payload: &'a str,
+    pub r#type: &'a str,
+}
+
 impl<'a> RtcpFb<'a> {
     pub(crate) fn new(value: &'a str) -> Result<Self> {
         let mut split = value.split(' ');
@@ -222,6 +205,23 @@ impl<'a> RtcpFb<'a> {
 
         Ok(Self { payload, r#type })
     }
+}
+
+/// SSSRC
+///
+/// a=ssrc:3570614608 cname:4TOk42mSjXCkVIa6
+///
+/// The cname source attribute associates a media source with its Canonical End-Point
+/// Identifier which will remain constant for the RTP media stream even when the ssrc
+/// identifier changes if a conflict is found. This is the value that the media sender
+/// will place in its RTCP SDES packets.
+#[derive(Debug, Default, Serialize, PartialEq)]
+pub struct Ssrc<'a> {
+    pub id: u64,
+    pub attribute: &'a str,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub value: Option<&'a str>,
 }
 
 impl<'a> Ssrc<'a> {
